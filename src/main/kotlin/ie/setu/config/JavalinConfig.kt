85710471c9622cc3b998da3abdd19f355dc30c0e
Bert Cross <bcros@hotmail.com>
2022-10-16 20:00:06 -0400

    Implement Lab5 updates/corrections

--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -1,2 +1,42 @@
 package ie.setu.config
 
+import ie.setu.controllers.HealthTrackerController
+import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.*
+
+class JavalinConfig {
+
+
+    fun startJavalinService(): Javalin {
+
+        val app = Javalin.create().apply {
+            exception(Exception::class.java) { e, _ -> e.printStackTrace() }
+            error(404) { ctx -> ctx.json("404 - Not Found") }
+        }.start(getHerokuAssignedPort())
+
+        registerRoutes(app)
+        return app
+    }
+    private fun registerRoutes(app: Javalin) {
+        app.routes {
+            path("/api/users") {
+                get(HealthTrackerController::getAllUsers)
+                post(HealthTrackerController::addUser)
+                path("{user-id}"){
+                    get(HealthTrackerController::getUserByUserId)
+                    delete(HealthTrackerController::delete)
+                    patch(HealthTrackerController::updateUser)
+                }
+                path("email/{email}") {
+                    get(HealthTrackerController::getUserByEmail)
+                }
+            }
+        }
+    }
+    private fun getHerokuAssignedPort(): Int {
+        val herokuPort = System.getenv("PORT")
+        return if (herokuPort != null) {
+            Integer.parseInt(herokuPort)
+        } else 7000
+    }
+}
--- src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
+++ src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
@@ -1,2 +1,38 @@
 package ie.setu.domain.repository
 
+import ie.setu.domain.User
+
+class UserDAO {
+
+    private val users = arrayListOf<User>(
+        User(name = "Geddy Lee", email = "geddy.lee@rush.com", id = 0),
+        User(name = "Alex Lifeson", email = "alex.lifeson@rush.com", id = 1),
+        User(name = "Neil Peart", email = "Neil.Peart@rush.com", id = 2),
+        User(name = "Eddie Van Halen", email = "Eddie@vanhalen.com", id = 3)
+    )
+
+    fun getAll() : ArrayList<User>{
+        return users
+    }
+    fun findById(id: Int): User?{
+        return users.find {it.id == id}
+    }
+    fun save(user: User){
+        users.add(user)
+    }
+    fun delete(id: Int) {
+        val user = findById(id)
+        users.remove(user)
+    }
+    fun findByEmail(email: String) :User?{
+        return users.find {it.email == email}
+    }
+
+
+    fun update(id: Int, user: User){
+        val foundUser = findById(id)
+        foundUser?.email = user.email
+        foundUser?.name = user.name
+        foundUser?.id = user.id
+    }
+}
