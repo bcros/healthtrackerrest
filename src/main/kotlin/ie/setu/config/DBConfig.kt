77a4943ccc809831d8add30cf6e11b9d8c589012
Bert Cross <bcros@hotmail.com>
2022-11-08 19:07:42 -0500

    Switch to Elephant DB

--- src/main/kotlin/ie/setu/config/DBConfig.kt
+++ src/main/kotlin/ie/setu/config/DBConfig.kt
@@ -13,11 +13,34 @@ class DbConfig{
 
         logger.info{"Starting DB Connection..."}
 
-        val dbConfig = Database.connect(
-            "jdbc:postgresql://ec2-44-210-36-247.compute-1.amazonaws.com:5432/ddjlssefdo9c85?sslmode=require",
-            driver = "org.postgresql.Driver",
-            user = "ergddswteehqpd",
-            password = "296121113cd1f3a63132b8ffbb8cd13a68ed291d3ad4e25c2b7a2cabba3d9dee")
+//        val dbConfig = Database.connect(
+//            "jdbc:postgresql://ec2-44-210-36-247.compute-1.amazonaws.com:5432/ddjlssefdo9c85?sslmode=require",
+//            driver = "org.postgresql.Driver",
+//            user = "ergddswteehqpd",
+//            password = "296121113cd1f3a63132b8ffbb8cd13a68ed291d3ad4e25c2b7a2cabba3d9dee")
+
+        val logger = KotlinLogging.logger {}
+        logger.info{"Starting DB Connection..."}
+
+        val PGUSER = "yrketawb"
+        val PGPASSWORD = "Nx2-stuK0ZudpI6HoLZgxpZb0P9TAbdr"
+        val PGHOST = "jelani.db.elephantsql.com"
+        val PGPORT = "5432"
+        val PGDATABASE = "yrketawb"
+
+        //url format should be jdbc:postgresql://host:port/database
+        val url = "jdbc:postgresql://$PGHOST:$PGPORT/$PGDATABASE"
+
+        val dbConfig = Database.connect(url,
+            driver="org.postgresql.Driver",
+            user = PGUSER,
+            password = PGPASSWORD
+        )
+
+        logger.info{"db url - connection: " + dbConfig.url}
+
+        return dbConfig
+
 
         logger.info{"DbConfig name = " + dbConfig.name}
         logger.info{"DbConfig url = " + dbConfig.url}
--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -1,55 +1,28 @@
 package ie.setu.config
 
 import ie.setu.controllers.HealthTrackerController
+import ie.setu.utils.jsonObjectMapper
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.*
-
-
-package io.javalin.example.kotlin
-
-import io.javalin.Javalin
-import io.javalin.apibuilder.ApiBuilder.*
-import io.javalin.example.kotlin.user.UserController
-import io.javalin.openapi.plugin.OpenApiConfiguration
-import io.javalin.openapi.plugin.OpenApiPlugin
-import io.javalin.openapi.plugin.redoc.ReDocConfiguration
-import io.javalin.openapi.plugin.redoc.ReDocPlugin
-import io.javalin.openapi.plugin.swagger.SwaggerConfiguration
-import io.javalin.openapi.plugin.swagger.SwaggerPlugin
-
-fun javalin_main() {
-
-    Javalin.create { config ->
-        config.plugins.register(OpenApiPlugin(OpenApiConfiguration().apply {
-            info.title = "Javalin OpenAIP example"
-        }))
-        config.plugins.register(SwaggerPlugin(SwaggerConfiguration()))
-        config.plugins.register(ReDocPlugin(ReDocConfiguration()))
-    }.routes {
-        path("users") {
-            get(UserController::getAll)
-            post(UserController::create)
-            path("{userId}") {
-                get(UserController::getOne)
-                patch(UserController::update)
-                delete(UserController::delete)
-            }
-        }
-    }.start(7001)
-
-    println("Check out ReDoc docs at http://localhost:7001/redoc")
-    println("Check out Swagger UI docs at http://localhost:7001/swagger")
-
-}
+import io.javalin.plugin.json.JavalinJackson
+import io.javalin.plugin.openapi.OpenApiOptions
+import io.javalin.plugin.openapi.OpenApiPlugin
+import io.javalin.plugin.openapi.ui.ReDocOptions
+import io.javalin.plugin.openapi.ui.SwaggerOptions
+import io.swagger.v3.oas.models.info.Info
 
 class JavalinConfig {
 
-
     fun startJavalinService(): Javalin {
-
-        val app = Javalin.create().apply {
+        val app = Javalin.create {
+            it.registerPlugin(getConfiguredOpenApiPlugin())
+            it.defaultContentType = "application/json"
+            //added this jsonMapper for our integration tests - serialise objects to json
+            it.jsonMapper(JavalinJackson(jsonObjectMapper()))
+            it.enableWebjars()
+        }.apply {
             exception(Exception::class.java) { e, _ -> e.printStackTrace() }
-            error(404) { ctx -> ctx.json("404 - Not Found") }
+            error(404) { ctx -> ctx.json("404 : Not Found") }
         }.start(getHerokuAssignedPort())
 
         registerRoutes(app)
@@ -77,4 +50,18 @@ class JavalinConfig {
             Integer.parseInt(herokuPort)
         } else 7000
     }
+
+    fun getConfiguredOpenApiPlugin() = OpenApiPlugin(
+        OpenApiOptions(
+            Info().apply {
+                title("Health Tracker App")
+                version("1.0")
+                description("Health Tracker API")
+            }
+        ).apply {
+            path("/swagger-docs") // endpoint for OpenAPI json
+            swagger(SwaggerOptions("/swagger-ui")) // endpoint for swagger-ui
+            reDoc(ReDocOptions("/redoc")) // endpoint for redoc
+        }
+    )
 }
